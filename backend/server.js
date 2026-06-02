9bb9fe8fe4db2174922c24f9ff25db1d630a3798
navyasowdala-sudo <navyasowdala@gmail.com>
2026-06-02 10:12:58 +0530

    Fix CORS origin

--- backend/server.js
+++ backend/server.js
@@ -19,15 +19,8 @@ require("dotenv").config();
 const app = express();
 connectDB();
 
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://online-food-app-w5li.vercel.app"
-  ],
-  credentials: true
-}
+app.use(cors());
 
-));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/foods", foodRoutes);
